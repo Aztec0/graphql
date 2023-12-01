fd0ad9e4ec45126664aefcd125bc85a45cacd23a
Aztec0 <atupota17@gmail.com>
2023-12-01 12:19:38 +0200

    update some files

--- app/scripts/main.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('gitUserForm').addEventListener('submit', function(event) {
-  event.preventDefault();
-  const gitLogin = document.getElementById('gitLogin').value;
-  localStorage.setItem('gitLogin', gitLogin);
-  window.location.href = 'result.html';
-});
